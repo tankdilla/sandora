39230e8de3f98a2e9003984dcce8d1eee7e7420b
darrell <darrellj@gmail.com>
2010-12-29 15:15:43 -0600

    rolling back a bit, some features not ready...

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ SandoraMysql::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "products#index"
+  root :to => "navigation#home"
 
   # See how all your routes lay out with "rake routes"
 
